7a35bdc9552b1645ad9989441697aaf0e4bee65e
Stef Smeets <stef.smeets@mmk.su.se>
2016-04-13 11:01:18 +0200

    Rearrange project src tree

--- build_all.sh
+++ build_all.sh
@@ -2,39 +2,31 @@
 
 base=`pwd`
 
-rm -rf ./focus_package/
+rm -rf ./bin/
 
 cd $base/focus
 sh focus_build_script.sh
 
-cd $base/kriber
-sh kriber_build_script.sh
-
-cd $base/dls76
-sh dls_build_script.sh
-
-cd $base/utils
+cd $base/src/kriber
+make clean
 make
+
+cd $base/src/dls76
+make clean
+make
+
+# cd $base/utils
+# make
 
 cd $base
 
-pkg="focus_package"
-mkdir $pkg
-mkdir $pkg/bin
+mkdir bin/
+mkdir bin/bin
 
-mv kriber/kriber_f $pkg
-mv dls76/dls_f $pkg
-
-cp kriber/pykriber.py      $pkg/
-cp dls76/pydls.py          $pkg/
-cp utils/coseq_cmp         $pkg/bin/
-cp utils/coseq_reduce      $pkg/bin/
-cp utils/genseq            $pkg/bin/
-cp utils/section           $pkg/bin/
-cp scripts/*               $pkg/bin/
-mv $pkg/bin/multifocal.py  $pkg/
-mv focus/build/exe/*       $pkg/bin/
-touch $pkg/__init__.py
+mv kriber/src/kriber.x    bin/bin
+mv dls76/src/dls76.x      bin/bin
+mv focus/build/exe/focus  bin/
+mv focus/build/exe/sginfo bin/
 
 echo
 echo "To install: python setup.py install"
