170abbc82a7ea8584be3cc1bc9dc99ad7567882b
Stef Smeets <stef.smeets@mmk.su.se>
2016-04-13 10:04:40 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,28 @@
 
 from setuptools import setup, find_packages
 from os import path
+import sys
+
+if sys.platform == "win32":
+    package_dir={"bin": "bin_windows/bin"}
+elif sys.platform == "darwin":
+    package_dir={"bin": "bin_osx/bin"}
+elif sys.platform == "linux2":
+    package_dir={"bin": "bin_linux/bin"}
+else:
+    raise RuntimeError
+
+print package_dir
 
 setup(
     name="focus_package",
-    version="1.0",
+    version="1.1",
     description="FOCUS package including DLS-76 and KRIBER",
 
     author="Stef Smeets",
-    author_email="stef.smeets@mat.ethz.ch",
+    author_email="stef.smeets@mmk.su.se",
     maintainer="Stef Smeets",
-    maintainer_email="stef.smeets@mat.ethz.ch",
+    maintainer_email="stef.smeets@mmk.su.se",
     license="GPL",
     url="https://github.com/stefsmeets/focus_package",
 
@@ -19,38 +31,60 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
 
-    packages=["focus_package"],
+    package_dir=package_dir,
+
+    packages=["focus_tools", "bin", "resources"],
 
     install_requires=[],
 
     package_data={
         "": ["LICENCE", "readme.md"],
-        "focus_package": ["*.py", "dls_f/*", "kriber_f/*"]
+        "focus_tools": ["*.py"],
+        "resources": ["*"],
+        "bin": ["*"]
     },
-
-    scripts=["focus_package/bin/cdlsall",
-             "focus_package/bin/coseq_cmp",
-             "focus_package/bin/coseq_reduce",
-             "focus_package/bin/dlsall",
-             "focus_package/bin/fo2hist",
-             "focus_package/bin/focus",
-             "focus_package/bin/focus2cif",
-             "focus_package/bin/focus2cm",
-             "focus_package/bin/focus2strudat",
-             "focus_package/bin/focus_check",
-             "focus_package/bin/genseq",
-             "focus_package/bin/get_coseq",
-             "focus_package/bin/section",
-             "focus_package/bin/sginfo",
-             "focus_package/bin/strudat2cif"],
 
     entry_points={
         'console_scripts': [
-            'multifocal = focus_package.multifocal:main',
-            'dls76      = focus_package.pydls:main',
-            'kriber     = focus_package.pykriber:main',
+            'multifocal  = focus_tools.multifocal:main',
+            'dls76       = focus_tools.pydls:dls76_entry',
+            'kriber      = focus_tools.pykriber:kriber_entry',
+            'strudat2cif = focus_tools.pykriber:strudat2cif_entry',
+            'fo2hist     = focus_tools.focus_tools:fo2hist_entry',
+            'fo2cif      = focus_tools.focus_tools:fo2cif_entry',
+            'fo2strudat  = focus_tools.focus_tools:fo2strudat_entry',
+            'dlsall      = focus_tools.focus_tools:dlsall_entry',
+            'cdlsall     = focus_tools.focus_tools:cdlsall_entry',
         ]
     }
 )
 
-print "\nInstallation complete. You can now remove this directory."
+if sys.platform == "win32":
+    print """
+ >> Installation complete. You can now remove this directory.
+
+Put these files in a location on your system path:
+
+    ./bin_windows/libfftw3f-3.dll
+    ./bin_windows/focus.exe
+    ./bin_windows/sginfo.exe
+"""
+else:    
+    print """
+ >> Installation complete. You can remove this directory.
+
+Put these files in a location on your system path:
+"""
+    if sys.platform == "darwin"
+        print """     
+        ./bin_osx/focus
+        ./bin_osx/sginfo"""
+    elif sys.platform == "linux2":
+        print """     
+        ./bin_linux/focus
+        ./bin_linux/sginfo"""
+
+    
+
+
+
